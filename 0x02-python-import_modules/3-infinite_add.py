10ff69ad9af762feaa7469e8760cec9fca0a36f5
Amine Ennadi <aminework229@gmail.com>
2024-01-23 01:30:30 +0100

    NeXt

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    number = len(sys.argv) - 1
-    if (number == 0):
-        print("0")
-    if (number == 1):
-        print("{}".format(sys.argv[1]))
-    else:
-        sum = 0
-        for i in range(len(sys.argv) -1):
-            sum += int(sys.argv[i + 1])
-        print(sum)
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print(sum)
