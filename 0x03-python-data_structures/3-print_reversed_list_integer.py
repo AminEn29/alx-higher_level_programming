892f23beb00e520886156ca873caab686f940723
Amine Ennadi <aminework229@gmail.com>
2024-01-25 12:56:54 +0100

    Task4

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/bin/bash/pythoon 
+def print_reversed_list_integer(my_list=[]):
+    a, c = len(my_list), 0
+    for b in range(a):
+        c = my_list[a - 1]
+        print("{:d}".format(c))
+        a = a - 1
