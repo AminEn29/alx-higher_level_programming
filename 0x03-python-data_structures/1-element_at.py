51ab55efb7325ac10000666f32e70d9b26d578de
Amine Ennadi <aminework229@gmail.com>
2024-01-25 11:04:09 +0100

    Task2

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+def element_at(my_list, idx):
+    a = len(my_list)
+    if idx > a:
+        return
+    if idx < 0:
+        return
+    for i in range(a):
+        if i == idx:
+            return (my_list[i])
