9c70306ea0805cba0fa78d1df2fe0540a008ca05
AminEn29 <138697936+AminEn29@users.noreply.github.com>
2024-03-18 22:19:24 +0000

    Delete 0x03-python-data_structures directory

--- 0x03-python-data_structures/0-print_list_integer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    a, b, c = len(my_list), 0, 0
-    for b in range(a):
-        c = my_list[b]
-        print("{:d}".format(c))
-        b + 1
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    a, c = len(my_list), 0
-    for b in range(a):
-        c = my_list[a - 1]
-        print("{:d}".format(c))
-        a = a - 1
